8d8c719c3bbdbc24fa2d9742f7097c3510bb5632
Tanesh <arvesh1701gmail.comarvesh1701gmail.com>
2022-05-01 13:03:27 +0400

    Added draggable components

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ function Footer() {
   const year = new Date().getFullYear();
   return (
     <footer>
-      <p>Copyright ⓒ {year}</p>
+      <p>Made By Tanesh in {year}</p>
     </footer>
   );
 }
--- src/components/Note.js
+++ src/components/Note.js
@@ -1,14 +1,25 @@
 import React from "react";
+import Draggable from 'react-draggable';
+import LeaderLine, { positionByWindowResize } from "react-leader-line";
 
 
+function drawLines() {
+  var startElement = document.getElementById('1'),
+  endElement = document.getElementById('2');
+ 
+// New leader line has red color and size 8.
+  new LeaderLine(startElement, endElement, {color: 'black', size: 4});
+}
 
 function Note(props) {
   return (
-    <div class="note">
+    <Draggable>
+    <div onClick={drawLines} id={props.identifier} className="note">
       <h1>{props.title}</h1>
       <p>{props.content}</p>
       {/* {notes.map(createNotes)} */}
     </div>
+    </Draggable>
   );
 }
 
