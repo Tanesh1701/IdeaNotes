020a42bb9589655df68d6a20e311ea64d7851c81
Tanesh <arvesh1701gmail.comarvesh1701gmail.com>
2022-05-02 15:49:04 +0400

    Removed redundant code

--- src/components/test.js
+++ src/components/test.js
@@ -1,28 +1,31 @@
-import Draggable from 'react-draggable';
-import React from 'react';
-import { useXarrow } from 'react-xarrows';
+// import Draggable from 'react-draggable';
+// import React from 'react';
+// import { useXarrow } from 'react-xarrows';
 
-export const boxStyle = {
-  border: '1px #999 solid',
-  borderRadius: '10px',
-  textAlign: 'center',
-  width: '100px',
-  height: '30px',
-  color: 'black',
-  alignItems: 'center',
-  display: 'flex',
-  justifyContent: 'center',
-}
+// export const boxStyle = {
+//   border: '1px #999 solid',
+//   borderRadius: '10px',
+//   textAlign: 'center',
+//   width: '100px',
+//   height: '30px',
+//   color: 'black',
+//   alignItems: 'center',
+//   display: 'flex',
+//   justifyContent: 'center',
+// }
 
-export const DraggableBox = ({ initialOffset = undefined, reference = undefined, id = "box2", ...style }) => {
-  const updateXarrow = useXarrow();
-  let moreStyle = {};
-  if (initialOffset) moreStyle = { position: 'absolute', left: initialOffset.x, top: initialOffset.y };
-  return (
-    <Draggable onDrag={updateXarrow} onStop={updateXarrow}>
-      <div ref={reference} id={id} style={{ ...boxStyle, ...style, ...moreStyle }}>
-        {id}
-      </div>
-    </Draggable>
-  );
-};
+// export const DraggableBox = ({ initialOffset = undefined, reference = undefined, id = "box2", ...style }) => {
+//   const updateXarrow = useXarrow();
+//   let moreStyle = {};
+//   if (initialOffset) moreStyle = { position: 'absolute', left: initialOffset.x, top: initialOffset.y };
+//   return (
+//     <Draggable onDrag={updateXarrow} onStop={updateXarrow}>
+//       <div ref={reference} id={id} style={{ ...boxStyle, ...style, ...moreStyle }}>
+//         {id}
+//       </div>
+//     </Draggable>
+//   );
+// };
+
+
+//unnecessary code
